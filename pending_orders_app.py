d2de82ec2df61e44b26d3de29dc1c22d5768331c
tlnjckdtt <telanjackielyn@gmail.com>
2025-08-25 22:33:28 +0800

    Update pending_orders_app.py

--- pending_orders_app.py
+++ pending_orders_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
+import pandas
 
 # Write directly to the app
 st.title(f":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
@@ -10,7 +11,10 @@ st.write("""Orders that need to be filled.""")
 #name_on_order = st.text_input('Name on Smoothie: ')
 #st.write('The name on your Smoothie will be: ', name_on_order)
 
-session = get_active_session()
+# session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.orders").filter(col('ORDER_FILLED')==0).collect()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
